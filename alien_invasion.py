ca89fff41661cc6b39647c455da974e795a0276d
CodeWithSahil-alt <sahilkhan7070020486@gmail.com>
2025-08-08 17:33:20 +0530

    Game Over!

--- alien_invasion.py
+++ alien_invasion.py
@@ -34,9 +34,12 @@ class AlienInvasion:
 		"""Start the main loop for the game."""
 		while True:
 			self._check_events()
-			self.ship.update()
-			self._update_bullets()
-			self._update_aliens()
+
+			if self.stats.game_active:
+				self.ship.update()
+				self._update_bullets()
+				self._update_aliens()
+			
 			self._update_screen()
 
 	def _check_events(self):
@@ -135,20 +138,23 @@ class AlienInvasion:
 
 	def _ship_hit(self):
 		"""Respond to the ship being hit by an alien."""
+		if self.stats.ships_left > 0:
 
-		# Dcreament in ships_left.
-		self.stats.ships_left -= 1
+			# Decreament in ships_left.
+			self.stats.ships_left -= 1
 
-		# Get rid of any remaining alien and bullet.
-		self.aliens.empty()
-		self.bullets.empty()
+			# Get rid of any remaining alien and bullet.
+			self.aliens.empty()
+			self.bullets.empty()
 
-		# Create a new flet and centre the ship.
-		self._create_fleet()
-		self.ship.centre_ship()
+			# Create a new flet and centre the ship.
+			self._create_fleet()
+			self.ship.centre_ship()
 
-		#Pause
-		sleep(1)
+			#Pause
+			sleep(1)
+		else:
+			self.stats.game_active = False
 
 	def _check_aliens_bottom(self):
 		"""Check if any alien have reached the bottom of the screen."""
